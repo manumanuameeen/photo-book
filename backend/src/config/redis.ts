b9e6302cca2ca252392ef046bed40fa73435eec2
manumanuameeen <jerrykerry3806@gmail.com>
2025-11-04 11:56:13 +0530

    fix: install redis, morgan, cookie-parser and add error type

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -8,7 +8,7 @@ const redisClient = createClient({
 });
 
 redisClient.on("connect", () => console.log("✅ Redis connected successfully"));
-redisClient.on("error", (err) => console.error("❌ Redis connection error:", err));
+redisClient.on("error", (err:Error) => console.error("❌ Redis connection error:", err));
 
 redisClient.connect();
 
