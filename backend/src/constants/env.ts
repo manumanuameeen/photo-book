c500a65bea1fd219cd4a0ec5cf2c08086c67b769
manumanuameeen <jerrykerry3806@gmail.com>
2025-12-08 12:14:42 +0530

    Save work before switiching tp dev

--- backend/src/constants/env.ts
+++
@@ -1,4 +0,0 @@
-export const ENV = {
-  ACCESS_TOKEN_MAX_AGE: Number(process.env.ACCESS_TOKEN_MAX_AGE) || 15 * 60 * 1000,
-  REFRESH_TOKEN_MAX_AGE: Number(process.env.REFRESH_TOKEN_MAX_AGE) || 7 * 24 * 60 * 60 * 1000,
-} as const;
--- backend/src/constants/role.ts
+++
@@ -1,5 +0,0 @@
-export const ROLE ={
-    PHOTOGRAPHER:"photographer",
-    USER:"user",
-    ADMIN:"admin"
-}
